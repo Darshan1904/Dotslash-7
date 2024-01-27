cb0c7d5ad430901f9fcff86dc26aa7547eabbf3f
bharat <bhartbhammar3336@gmail.com>
2024-01-27 12:59:41 +0530

    HomePage added

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import Home from "./pages/Home";
+import Canvas from './canvas';
+function App() {
+    return (
+        <main className='app transition-all ease-in'>
+            <Home />
+            <Canvas />
+        </main>
+    );
+}
+
+export default App;
